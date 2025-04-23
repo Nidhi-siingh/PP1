7769f430a115687ce3acc0d06a196c6afd7aafd6
Nidhi <nidhisingh.officialid@gmail.com>
2025-04-23 11:09:01 +0530

    Initial commit

---
+++ PP1.py
@@ -0,0 +1,5 @@
+A = int (input("Enter your age: "))
+if A < 18:
+    print("Your age is less than 18")
+else:
+    print ("Your age is:", A)
---
+++ Project1.py
@@ -0,0 +1,1 @@
+print ("Hello Jenkin, This is my first project")
